2dcb0fd3d7cbca380a25cec7f0ec904bd567178a
PJ Frias <peejfrias@gmail.com>
2018-01-05 01:51:40 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,27 +4,21 @@ def third_challenge
       :patriarch => {
         :name => "Lord Montague",
         :age => "53"
-        #your key/value pairs here
         },
       :matriarch => {
         :name => "Lady Montague",
         :age => "54"
-        #your key/value pairs here
         },
       :hero => {
-        #your key/value pairs here
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
         },
       :matriarch => {
-        #your key/value pairs here
         },
       :heroine => {
-        #your key/value pairs here
         },
       :heroine_friends => []
    }
